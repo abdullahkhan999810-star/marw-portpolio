db1267867a80ead119924f46eecc8f5325e1ecea
abdullahkhan999810-star <abdullahkhan999810@gmail.com>
2026-05-24 13:44:41 +0500

    marwa portfolio

---
+++ java.js
@@ -0,0 +1,5 @@
+// Simple form message
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thanks for contacting Iqra Amjad! I will reply soon.");
+});
